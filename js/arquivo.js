e4c8bc562ebf2b96ded4e9467a6510d97db6af8d
julia031209 <juliadossantosferreira70@gmail.com>
2025-07-01 12:25:35 -0300

    descriçao

---
+++ js/arquivo.js
@@ -0,0 +1,26 @@
+var global = "Variavel global";
+console.log(global);
+
+function exemplo2(){
+    var local = "Variavel local";
+    console.log(local);
+}
+
+// console.log(local);
+exemplo2();
+
+let x = 10;
+
+if(x > 5){
+    let y = 20;
+    console.log("Valor de y: ", y);
+}
+console.log(x);
+// console.log(y);
+
+const_constante = "texto";
+console.log(_constante);
+
+function saudacao(){
+    alert("Bem vindo au nosso site ³");
+}
